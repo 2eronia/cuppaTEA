723585ba31f557a2c0b438fcc146637d70ef32d1
2eronia <51925751+2eronia@users.noreply.github.com>
2022-01-03 16:29:11 +0800

    Update goof_off.js

--- goof_off.js
+++ goof_off.js
@@ -1,2 +1,2 @@
 // [rule: 摸鱼]
-sendText(request("https://vps.suyiboke.com/api/face/my").replace(/:/g, "").replace("自己的", "自己的。\n").replace("上班是", "\n上班是").replace(/<br\/>/g, "\n"))
+sendText(request("https://vps.suyiboke.com/api/face/my").replace(/:/g, "").replace(",但命是自己的", "，但命是自己的。\n").replace("上班是", "\n上班是").replace(/<br\/>/g, "\n"))
